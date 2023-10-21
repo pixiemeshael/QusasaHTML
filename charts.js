eac0e4b5eeac306d2a99158235b92ca3ea6e6e34
emtenanag <emtenan.ag@gmail.com>
2023-10-21 09:16:41 +0300

    Home page + Responsivness

---
+++ charts.js
@@ -0,0 +1,14 @@
+/*
+I loved Virgil's Dribble shot and decided to remake
+it with some SVG animations :) I didn't really want
+to recreate the curvy line since I believe straight
+lines are more accurate. Sorry Virgil, I might make
+them curvy one day :)! With a bit of time on my 
+hands I could make it even more detailed.
+I hope you like it and post some feedback on how to
+make this better :)!
+
+Don't forget to check out the original at
+https://goo.gl/0aKiS0
+
+Kudos Virgil!*/
